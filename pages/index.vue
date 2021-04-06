efb4f421a31d80d42794d37f4e3ea448cd23c9cb
Jason Chafin <Herm71@users.noreply.github.com>
2021-04-06 09:04:58 -0700

    saving earlier work

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,36 @@
+
 <template>
-  <h1>Hello world!</h1>
+  <div class="container">
+    <div>
+      <!-- <HeaderGlobal /> -->
+      <h1 class="title">UCSC Pantheon Updates</h1>
+
+      <NewComponent />
+    </div>
+  </div>
 </template>
+
+<script>
+// import HeaderGlobal from "~/components/HeaderGlobal.vue";
+import NewComponent from "~/components/NewComponent.vue";
+
+export default {
+  components: { NewComponent },
+};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+}
+
+/* .container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+} */
+</style>
